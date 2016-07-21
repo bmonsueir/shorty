f0683b0ce3704c00f17251726038ae8d4c576986
Bill Monsueir <bmonsueir@gmail.com>
2016-07-21 08:37:00 -0700

    heroku

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ Router.configure({
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
-
+//
 import { Short } from '/imports/api/shorty.js';
 Router.map(function(){
     this.route('about', {path: '/'});
